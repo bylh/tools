1249949d85745fae013b08b07eb1cd170094b38f
lihuan <494397353@qq.com>
2020-08-13 00:25:11 +0800

    append

--- src/views/bit/index.vue
+++ src/views/bit/index.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="bit">
-        <div class="animate__heartBeat">测试</div>
+        <div class="animate__heartBeat">
+            <section class="section">
+                <div class="container">
+                    <h1 class="title">Section</h1>
+                    <h2 class="subtitle">
+                        A simple container to divide your page into
+                        <strong>sections</strong>, like the one you're currently reading
+                    </h2>
+                </div>
+            </section>
+        </div>
     </div>
 </template>
 
